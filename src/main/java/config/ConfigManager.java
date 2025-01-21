d427e49612ec39ee088664497088ce58a74276e0
Lorenz Moser <lorenz.moser.bw@gmail.com>
2025-01-21 13:50:52 +0100

    refactor: split VitruvServerApp into multiple classes for improved modularity

---
+++ src/main/java/config/ConfigManager.java
@@ -0,0 +1,29 @@
+package config;
+
+import java.io.InputStream;
+import java.util.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConfigManager {
+    private static final Logger logger = LoggerFactory.getLogger(ConfigManager.class);
+    private final Properties properties = new Properties();
+
+    public ConfigManager(String configFileName) throws Exception {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream(configFileName)) {
+            if (input == null) {
+                logger.error("Config file not found: " + configFileName);
+                throw new Exception("Config file not found: " + configFileName); // TODO: exception not necessary
+            }
+            properties.load(input);
+        }
+    }
+
+    public int getVitruvServerPort() {
+        return Integer.parseInt(properties.getProperty("vitruv-server.port", "8080"));
+    }
+
+    public int getHttpsServerPort() {
+        return Integer.parseInt(properties.getProperty("https-server.port", "8443"));
+    }
+}
---
+++ src/main/java/server/HttpsRequestHandler.java
@@ -0,0 +1,67 @@
+package server;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpsRequestHandler implements HttpHandler {
+
+    private final int forwardPort;
+    private static final Logger logger = LoggerFactory.getLogger(HttpsRequestHandler.class);
+
+
+    public HttpsRequestHandler(int forwardPort) {
+        this.forwardPort = forwardPort;
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) {
+        try {
+            logger.info("redirect to VitruvServer at port {}", this.forwardPort);
+            logger.info("Request URI: {}", exchange.getRequestURI().toString());
+
+            // connect to intern http VitruvServer
+            final String vitruvHost = "http://localhost:" + this.forwardPort; // TODO: configure domain
+            final String fullUri = vitruvHost + exchange.getRequestURI().toString();
+
+            // redirect HTTP request to Vitruv
+            final HttpURLConnection connection = (HttpURLConnection) new URL(fullUri).openConnection();
+            connection.setRequestMethod(exchange.getRequestMethod());
+            exchange.getRequestHeaders().forEach((key, values) -> {
+                for (String value : values) {
+                    connection.setRequestProperty(key, value);
+                }
+            });
+
+            // redirect body
+            if (exchange.getRequestBody().available() > 0) {
+                connection.setDoOutput(true);
+                try (OutputStream os = connection.getOutputStream()) {
+                    os.write(exchange.getRequestBody().readAllBytes());
+                }
+            }
+
+            // read answer
+            final int responseCode = connection.getResponseCode();
+            InputStream responseStream = responseCode >= 400
+                    ? connection.getErrorStream()
+                    : connection.getInputStream();
+
+            // return answer to client
+            exchange.sendResponseHeaders(responseCode, responseStream.available());
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(responseStream.readAllBytes());
+            }
+            logger.info("Response code: {}", responseCode);
+        }
+        catch (Exception e) {
+            logger.error("An error occurred while handling the HTTP request: {}", e.getMessage(), e);
+        }
+    }
+}
