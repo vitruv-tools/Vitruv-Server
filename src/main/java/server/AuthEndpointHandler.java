fd982e66652a4e9feb33c6494ade05f2331e37d4
Lorenz Moser <lorenz.moser.bw@gmail.com>
2025-01-22 16:10:33 +0100

    add class for auth endpoint

---
+++ src/main/java/server/AuthEndpointHandler.java
@@ -0,0 +1,30 @@
+package server;
+
+import app.VitruvServerApp;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class AuthEndpointHandler implements HttpHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthEndpointHandler.class);
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        try {
+            String authorizationUrl = VitruvServerApp.getOidcClient().getAuthorizationRequestURI().toString();
+            logger.info("SSO redirect to authorization URL: {}", authorizationUrl);
+            exchange.getResponseHeaders().set("Location", authorizationUrl);
+            // code 302 for redirect, -1 for no body
+            exchange.sendResponseHeaders(302, -1);
+        } catch (Exception e) {
+            logger.error("Error generating authorization URL: {}", e.getMessage());
+            exchange.sendResponseHeaders(500, 0);
+        } finally {
+            exchange.close();
+        }
+    }
+}
