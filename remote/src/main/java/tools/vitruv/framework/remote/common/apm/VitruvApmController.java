9ff10b695fb07f7be83a772787f1daaad5e156ba
Muhammad Zeeshan Tahir <muhammad.tahir@kit.edu>
2025-02-18 01:07:29 +0100

    Update VitruvApmController.java

--- remote/src/main/java/tools/vitruv/framework/remote/common/apm/VitruvApmController.java
+++ remote/src/main/java/tools/vitruv/framework/remote/common/apm/VitruvApmController.java
@@ -9,28 +9,40 @@ import io.micrometer.core.instrument.Metrics;
  * This class allows controlling the Vitruvius monitoring.
  */
 public class VitruvApmController {
-	private static VitruvStepMeterRegistry ACTIVE_REGISTRY;
-	
-	/**
-	 * Enables the monitoring for Vitruvius.
-	 * 
-	 * @param output Path to a file in which all measurements are stored.
-	 */
-	public static void enable(Path output) {
-		if (ACTIVE_REGISTRY == null) {
-			ACTIVE_REGISTRY = new VitruvStepMeterRegistry(new VitruvStepRegistryConfig(), Clock.SYSTEM, output);
-			Metrics.globalRegistry.add(ACTIVE_REGISTRY);
-		}
-	}
-	
-	/**
-	 * Disables the monitoring for Vitruvius.
-	 */
-	public static void disable() {
-		if (ACTIVE_REGISTRY != null) {
-			ACTIVE_REGISTRY.stop();
-			Metrics.globalRegistry.remove(ACTIVE_REGISTRY);
-			ACTIVE_REGISTRY = null;
-		}
-	}
+    
+	private static VitruvStepMeterRegistry activeRegistry;
+
+    /**
+     * Constructor to initialize the monitoring.
+     * 
+     * @param output Path to a file in which all measurements are stored.
+     */
+    private  VitruvApmController(Path output) {
+         if (ACTIVE_REGISTRY == null) {
+            ACTIVE_REGISTRY = new VitruvStepMeterRegistry(new VitruvStepRegistryConfig(), Clock.SYSTEM, output);
+        }
+    }
+    
+    /**
+     * Enables the monitoring for Vitruvius.
+     * 
+     * @param output Path to a file in which all measurements are stored.
+     */
+    public static void enable(Path output) {
+        if (activeRegistry == null) {
+            new VitruvApmController(output);  // This calls the private constructor
+            Metrics.globalRegistry.add(activeRegistry);
+        }
+    }
+    
+    /**
+     * Disables the monitoring for Vitruvius.
+     */
+    public static void disable() {
+        if (activeRegistry != null) {
+            activeRegistry.stop();
+            Metrics.globalRegistry.remove(activeRegistry);
+            activeRegistry = null;
+        }
+    }
 }
