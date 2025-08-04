4582c353d4e7191140e118ef0566c46e0d2ec99c
Martin Armbruster <armbrustermartin@googlemail.com>
2025-08-04 17:24:05 +0200

    Moved the actual HTTP request / response handling to an abstract class.

---
+++ remote/src/main/java/tools/vitruv/framework/remote/server/http/AbstractHttpHandler.java
@@ -0,0 +1,54 @@
+package tools.vitruv.framework.remote.server.http;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import tools.vitruv.framework.remote.common.rest.constants.ContentType;
+import tools.vitruv.framework.remote.server.exception.ServerHaltingException;
+import tools.vitruv.framework.remote.server.rest.PathEndointCollector;
+
+public abstract class AbstractHttpHandler {
+    private static final int HTTP_NOT_FOUND = 404;
+    private static final int HTTP_OK = 200;
+    private static final int HTTP_INTERNAL_ERROR = 500;
+
+    private PathEndointCollector endpoints;
+
+    protected AbstractHttpHandler(PathEndointCollector endpoints) {
+    	this.endpoints = endpoints;
+    }
+
+    /**
+     * Processes an request when this end point is called.
+     *
+     * @param exchange An object encapsulating the HTTP request and response.
+     */
+    protected void process(String method, HttpWrapper wrapper) {
+        try {
+            var response = switch (method) {
+                case "GET" -> endpoints.getEndpoint().process(wrapper);
+                case "PUT" -> endpoints.putEndpoint().process(wrapper);
+                case "POST" -> endpoints.postEndpoint().process(wrapper);
+                case "PATCH" -> endpoints.patchEndpoint().process(wrapper);
+                case "DELETE" -> endpoints.deleteEndpoint().process(wrapper);
+                default -> throw new ServerHaltingException(HTTP_NOT_FOUND, "Request method not supported!");
+            };
+            if (response != null) {
+                wrapper.sendResponse(HTTP_OK, response.getBytes(StandardCharsets.UTF_8));
+            } else {
+                wrapper.sendResponse(HTTP_OK);
+            }
+        } catch (Exception exception) {
+            var statusCode = HTTP_INTERNAL_ERROR;
+            if (exception instanceof ServerHaltingException haltingException) {
+                statusCode = haltingException.getStatusCode();
+            }
+            wrapper.setContentType(ContentType.TEXT_PLAIN);
+            try {
+            	wrapper.sendResponse(statusCode, exception.getMessage().getBytes(StandardCharsets.UTF_8));
+            } catch (IOException e) {
+            	throw new IllegalStateException("Sending a response (" + statusCode + " " + exception.getMessage() + ") failed.", e);
+            }
+        }
+    }
+}
--- remote/src/main/java/tools/vitruv/framework/remote/server/http/java/RequestHandler.java
+++ remote/src/main/java/tools/vitruv/framework/remote/server/http/java/RequestHandler.java
@@ -3,24 +3,15 @@ package tools.vitruv.framework.remote.server.http.java;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import tools.vitruv.framework.remote.common.rest.constants.ContentType;
-import tools.vitruv.framework.remote.server.exception.ServerHaltingException;
+import tools.vitruv.framework.remote.server.http.AbstractHttpHandler;
 import tools.vitruv.framework.remote.server.rest.PathEndointCollector;
-
-import java.nio.charset.StandardCharsets;
-
-import static java.net.HttpURLConnection.*;
-
-import java.io.IOException;
 
 /**
  * Represents an {@link HttpHandler}.
  */
-class RequestHandler implements HttpHandler {
-    private PathEndointCollector endpoints;
-
+class RequestHandler extends AbstractHttpHandler implements HttpHandler {
     RequestHandler(PathEndointCollector endpoints) {
-    	this.endpoints = endpoints;
+        super(endpoints);
     }
 
     /**
@@ -32,31 +23,6 @@ class RequestHandler implements HttpHandler {
     public void handle(HttpExchange exchange) {
         var method = exchange.getRequestMethod();
         var wrapper = new HttpExchangeWrapper(exchange);
-        try {
-            var response = switch (method) {
-                case "GET" -> endpoints.getEndpoint().process(wrapper);
-                case "PUT" -> endpoints.putEndpoint().process(wrapper);
-                case "POST" -> endpoints.postEndpoint().process(wrapper);
-                case "PATCH" -> endpoints.patchEndpoint().process(wrapper);
-                case "DELETE" -> endpoints.deleteEndpoint().process(wrapper);
-                default -> throw new ServerHaltingException(HTTP_NOT_FOUND, "Request method not supported!");
-            };
-            if (response != null) {
-                wrapper.sendResponse(HTTP_OK, response.getBytes(StandardCharsets.UTF_8));
-            } else {
-                wrapper.sendResponse(HTTP_OK);
-            }
-        } catch (Exception exception) {
-            var statusCode = HTTP_INTERNAL_ERROR;
-            if (exception instanceof ServerHaltingException haltingException) {
-                statusCode = haltingException.getStatusCode();
-            }
-            wrapper.setContentType(ContentType.TEXT_PLAIN);
-            try {
-            	wrapper.sendResponse(statusCode, exception.getMessage().getBytes(StandardCharsets.UTF_8));
-            } catch (IOException e) {
-            	throw new IllegalStateException("Sending a response (" + statusCode + " " + exception.getMessage() + ") failed.", e);
-            }
-        }
+        this.process(method, wrapper);
     }
 }
