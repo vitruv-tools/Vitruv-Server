4967384becb6ab2f7d23f5fa43a11c0faf27fc12
Arne Lange <arne.lange@kit.edu>
2025-12-09 16:53:09 +0100

    implemented the getMetaModel() method

--- remote/src/main/java/tools/vitruv/framework/remote/client/impl/RemoteViewType.java
+++ remote/src/main/java/tools/vitruv/framework/remote/client/impl/RemoteViewType.java
@@ -1,20 +1,25 @@
 package tools.vitruv.framework.remote.client.impl;
 
+import org.eclipse.emf.ecore.EPackage;
+
 import tools.vitruv.framework.views.ChangeableViewSource;
 import tools.vitruv.framework.views.ViewSelector;
 import tools.vitruv.framework.views.ViewType;
 
 /**
- * A Vitruvius view type representing actual types on the virtual model, but is still capable of providing a view selector and allows creating
+ * A Vitruvius view type representing actual types on the virtual model, but is
+ * still capable of providing a view selector and allows creating
  * views by querying the Vitruvius server.
  */
 public class RemoteViewType implements ViewType<ViewSelector> {
     private final String name;
     private final VitruvRemoteConnection remoteConnection;
+    private final EPackage metamodel;
 
-    RemoteViewType(String name, VitruvRemoteConnection remoteConnection) {
+    RemoteViewType(String name, VitruvRemoteConnection remoteConnection, EPackage metamodel) {
         this.name = name;
         this.remoteConnection = remoteConnection;
+        this.metamodel = metamodel;
     }
 
     @Override
@@ -23,13 +28,20 @@ public class RemoteViewType implements ViewType<ViewSelector> {
     }
 
     /**
-     * Returns the {@link ViewSelector} of the {@link ViewType}, which allows configuring views by delegating the request to the Vitruvius server.
+     * Returns the {@link ViewSelector} of the {@link ViewType}, which allows
+     * configuring views by delegating the request to the Vitruvius server.
      * 
      * @param viewSource Ignored, can be null.
-     * @return A view selector for the view type represented by this remote view type.
+     * @return A view selector for the view type represented by this remote view
+     *         type.
      */
     @Override
     public ViewSelector createSelector(ChangeableViewSource viewSource) {
         return remoteConnection.getSelector(name);
+    }
+
+    @Override
+    public EPackage getMetamodel() {
+        return this.metamodel;
     }
 }
